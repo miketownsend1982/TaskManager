e5afebab3926eee09b6e3b3dfb6fda069c8d0509
Mike Townsend <miketownsend@Mikes-MacBook-Pro.local>
2023-02-26 10:41:25 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+## TaskManager
+
+Project 1 of [Node.js / Express Course - Build 4 Projects](https://www.youtube.com/watch?v=qwfE7fSVaZM)
+
+Requirements are:
+
+- Create an .env file with **MONGO_URI** var
+- npm start
+- PORT=5000 node app.js
---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+// middleware
+const notFound = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+app.use(express.json())
+
+// routes
+app.use(express.static("./public"))
+
+app.use('/api/v1/tasks', tasks)
+
+app.use(notFound)
+app.use(errorHandlerMiddleware);
+// app.get('/api/v1/tasks') - get all tasks
+// app.post('/api/v1/tasks') - create a new task
+// app.get('/api/v1/tasks/:id') - get single task
+// app.patch('/api/v1/tasks/:id') - update task
+// app.delete('/api/v1/tasks/:id') - delete task
+
+const port = process.env.PORT
+
+
+const start = async() =>{
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port,console.log(`server is listening ${port}`))
+    } catch (error){
+        console.log(error)
+    }
+}
+
+start()
+ 
+
+
+
+
